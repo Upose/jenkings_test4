332eb65174fdbb42b51c2ec399b89af6cf4ecb36
904678748@qq.com <904678748@qq.com>
2021-12-13 11:50:34 +0800

    .

--- src/components/admin/program/program_manage.vue
+++ src/components/admin/program/program_manage.vue
@@ -28,7 +28,7 @@
             </el-table-column>
           </el-table> -->
 
-          <el-table :data="tableData" style="width: 100%">
+          <el-table :data="tableData" style="width: 100%" :expand-row-keys="expends" :row-key="getRowKeys">
             <el-table-column type="expand" width="30">
               <template slot-scope="scope">
                 <div v-for="(item,index) in scope.row.plateList" class="row c-l">
@@ -36,7 +36,7 @@
                   <div class="col2">{{item.name}}</div>
                   <div class="col3"></div>
                   <div class="col4">{{item.createTime.slice(0,10)}}</div>
-                  <div class="col5"><el-button @click="handleSet(scope.row)" type="text" size="mini" icon="el-icon-setting" round>栏目设置</el-button></div>
+                  <div class="col5"><el-button @click="handleSet(item)" type="text" size="mini" icon="el-icon-setting" round>栏目设置</el-button></div>
                 </div>
               </template>
             </el-table-column>
@@ -53,8 +53,103 @@
     </el-container>
   </div>
 </template>
+
+<script>
+import footerPage from "@/components/admin/common/footer";
+import breadcrumb from "@/components/admin/common/breadcrumb";
+import serviceLMenu from "@/components/admin/common/serviceLMenu";
+export default {
+  name: 'index',
+  created(){
+    this.bus.$on('collapse', msg => {
+      this.$root.collapse = msg;
+    })
+  },
+  components:{footerPage,serviceLMenu,breadcrumb},
+  data () {
+    return {
+      tab_name:['1','2','3'],
+      activeName:'',
+      m_index:0,//一级菜单点击位置
+      s_index:0,//二级菜单点击位置
+      dataList:[],//主菜单
+      s_list:[],//子菜单
+      tableData:[],
+      expends:[],
+    }
+  },
+  mounted(){
+    this.initData();
+  },
+  methods:{
+    getExpends(){
+      var appId = this.tableData.map(item => item.appId)
+      this.expends = appId;
+      console.log(this.expends);
+    },
+    getRowKeys(row){
+      return row.appId
+    },
+    //获取终端和终端对应的场景
+    initData(){
+      var _this = this;
+      this.http.getJsonSelf('scene-overview','?TopCount=100').then(res=>{ 
+        _this.dataList = res.data||[];
+        if(_this.dataList.length>0){
+          _this.activeName = _this.dataList[0].terminalId;
+          _this.s_list = _this.dataList[0].sceneList||[];
+          if(_this.s_list.length>0){
+            _this.getList(_this.s_list[0].id);
+          }
+        }
+      }).catch(err=>{
+          console.log(err);
+      })
+    },
+    //根据场景，获取对应的下方列表
+    getList(id){
+      this.http.getJsonSelf('app-plate-list-by-scene-id','/'+id).then(res=>{ 
+        this.tableData = res.data||[];
+        this.getExpends();
+      }).catch(err=>{
+
+      })
+    },
+    //一级菜单点击
+    handleClick(val){
+      this.m_index = val.index;
+      this.s_index = 0;
+      console.log(val.index);//下标
+    },
+    //二级菜单点击
+    menuClick(val,index){
+      this.s_index = index;
+      this.getList(val.id);
+    },
+    handleSet(row){
+      window.open(row.visitUrl, "_blank");
+    },
+  },
+}
+</script>
+
 <style lang="less" scoped>
-  .row{
+@import "../../../assets/admin/css/color.less";/**颜色配置 */
+  .content{
+    background-color: @fff;
+    border-radius: 4px;
+    box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.02);
+    /deep/.el-tabs__header{
+      margin: 0;
+    }
+    .search-div{
+      padding: 20px;
+    }
+    /deep/.tabs-check .el-tabs__content{
+      display: none;
+    }
+  }
+    .row{
     border-bottom: 1px solid #eee;
     padding: 8px 0;
     &:last-child{
@@ -84,92 +179,6 @@
       .el-button{
         margin-top: -5px;
       }
-    }
-  }
-</style>
-<script>
-import footerPage from "@/components/admin/common/footer";
-import breadcrumb from "@/components/admin/common/breadcrumb";
-import serviceLMenu from "@/components/admin/common/serviceLMenu";
-export default {
-  name: 'index',
-  created(){
-    this.bus.$on('collapse', msg => {
-      this.$root.collapse = msg;
-    })
-  },
-  components:{footerPage,serviceLMenu,breadcrumb},
-  data () {
-    return {
-      tab_name:['1','2','3'],
-      activeName:'',
-      m_index:0,//一级菜单点击位置
-      s_index:0,//二级菜单点击位置
-      dataList:[],//主菜单
-      s_list:[],//子菜单
-      tableData:[],
-    }
-  },
-  mounted(){
-      this.initData();
-  },
-  methods:{
-    //获取终端和终端对应的场景
-    initData(){
-      var _this = this;
-      this.http.getJsonSelf('scene-overview','?TopCount=100').then(res=>{ 
-        _this.dataList = res.data||[];
-        if(_this.dataList.length>0){
-          _this.activeName = _this.dataList[0].terminalId;
-          _this.s_list = _this.dataList[0].sceneList||[];
-          if(_this.s_list.length>0){
-            _this.getList(_this.s_list[0].id);
-          }
-        }
-      }).catch(err=>{
-          console.log(err);
-      })
-    },
-    //根据场景，获取对应的下方列表
-    getList(id){
-      this.http.getJsonSelf('app-plate-list-by-scene-id','/'+id).then(res=>{ 
-        this.tableData = res.data||[];
-      }).catch(err=>{
-
-      })
-    },
-    //一级菜单点击
-    handleClick(val){
-      this.m_index = val.index;
-      this.s_index = 0;
-      console.log(val.index);//下标
-    },
-    //二级菜单点击
-    menuClick(val,index){
-      this.s_index = index;
-      this.getList(val.id);
-    },
-    handleSet(row){
-      this.$router.push({path:'programAdd',query: {id:row.id,}});
-    },
-  },
-}
-</script>
-
-<style lang="less" scoped>
-@import "../../../assets/admin/css/color.less";/**颜色配置 */
-  .content{
-    background-color: @fff;
-    border-radius: 4px;
-    box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.02);
-    /deep/.el-tabs__header{
-      margin: 0;
-    }
-    .search-div{
-      padding: 20px;
-    }
-    /deep/.tabs-check .el-tabs__content{
-      display: none;
     }
   }
 </style>
