fbd5b285f89444c4a9f2ea71c8a759a6030a8fe6
904678748@qq.com <904678748@qq.com>
2022-01-12 17:32:59 +0800

    ..

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  VUE_APP_IMG_URL:'"http://192.168.21.71:5075"',//图片路径、域名前缀
+  VUE_APP_IMG_URL:'"http://192.168.21.46:6900"',//图片路径、域名前缀
   VUE_APP_template_url:'"http://192.168.21.71:9000"',//模板路径名
   VUE_APP_BASE_API:'"http://192.168.21.46:8000"',//请求地址
 })
