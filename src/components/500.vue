d12674153fd3a626f9bf77fa495e7b112fd2816d
904678748@qq.com <904678748@qq.com>
2022-03-24 18:16:02 +0800

    0

--- src/components/500.vue
+++ src/components/500.vue
@@ -6,7 +6,7 @@
           <span class="err-title">出错啦！</span>
           <span>抱歉，您访问的页面不存在。</span>
         </div>
-        <button class="go-flush" @click="window.history.go(-1);">返回上一页</button>
+        <button class="go-flush" @click="goBack()">返回上一页</button>
         <button class="go-home" @click="goHome()">回到首页</button>
       </div>
       <div class="img-404">
@@ -48,6 +48,9 @@ export default {
         let info = urlInfo.find(item => item.code == code);
         window.location.href = info.path + url;
       }
+    },
+    goBack(){
+      window.history.go(-1);
     },
     goHome(){
       this.linkTo(this.code,this.path);
