d2ab24ef2e7fa97647ceaa5d19181bd09d6c2a4b
904678748@qq.com <904678748@qq.com>
2022-08-11 17:38:23 +0800

    更改图标颜色

--- src/components/web/view/mode_preview.vue
+++ src/components/web/view/mode_preview.vue
@@ -12,6 +12,10 @@
     <br>
     <news_sys></news_sys>
     <br>
+    <activities_and_lectures></activities_and_lectures>
+    <br>
+    <database_nav_sys1></database_nav_sys1>
+    <br>
     <unified_retrieval_sys ></unified_retrieval_sys>
     <br>
   </div>
@@ -20,14 +24,16 @@
 <script>
 import academic_result from "@/components/web/model/scsf/academic_result";
 import database_nav_sys from "@/components/web/model/scsf/database_nav_sys";
+import database_nav_sys1 from "@/components/web/model/scsf/database_nav_sys1";
 import header_sys from "@/components/web/model/scsf/header_sys";
+import activities_and_lectures from "@/components/web/model/scsf/activities_and_lectures";
 import hot_services from "@/components/web/model/scsf/hot_services";
 import literature_project_sys from "@/components/web/model/scsf/literature_project_sys";
 import news_sys from "@/components/web/model/scsf/news_sys";
 import unified_retrieval_sys from "@/components/web/model/scsf/unified_retrieval_sys";
 export default {
   created() { },
-  components: { academic_result, database_nav_sys, header_sys, hot_services, literature_project_sys, news_sys, unified_retrieval_sys },
+  components: { academic_result, database_nav_sys,database_nav_sys1, header_sys, hot_services, literature_project_sys, news_sys,activities_and_lectures, unified_retrieval_sys },
   data() {
     return {
 
