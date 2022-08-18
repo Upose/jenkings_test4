2cf22888ed6c7f14f52482ebdee2ecf016fe3886
904678748@qq.com <904678748@qq.com>
2022-08-18 12:21:01 +0800

    统计数据

--- src/components/web/model/sxdx/footer_sys.vue
+++ src/components/web/model/sxdx/footer_sys.vue
@@ -1,7 +1,19 @@
 <!--底部模板-->
 <template>
-  <div class="list-warp">
-    底部模板
+  <div class="foot-warp">
+    <div class="m-width c-l">
+      <div class="left">1</div>
+      <div class="center">
+        <div class="coumn-w">
+          <a href="javascript:;" v-for="i in 7">山西大学</a>
+        </div>
+        <div class="edit-w">
+          <p>校区地址：山西省太原市小店区戊城路92号      邮箱：sxulib@edu.cn</p>
+          <p>Copyright©2020年 主页制作与维护;山西大学图书馆     建议浏览器使用IE10、Firefox、谷歌浏览</p>
+        </div><!--富文本-->
+      </div>
+      <div class="right">3</div>
+    </div>
   </div>
 </template>
 
@@ -22,5 +34,18 @@ export default {
 </script>
 
 <style lang="less" scoped>
-  
+.foot-warp{
+  .left,.center,.right{
+    float: left;
+  }
+  .left{
+    width: 20%;
+  }
+  .center{
+    width: 60%;
+  }
+  .right{
+    width: 20%;
+  }
+}
 </style>
