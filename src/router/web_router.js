ce31ec36bac4a9bf8fbf3a61c0818f37e213fbff
904678748@qq.com <904678748@qq.com>
2022-09-02 09:14:06 +0800

    ...

--- src/router/web_router.js
+++ src/router/web_router.js
@@ -1,11 +1,11 @@
 /***门户路由 */
 export default {
     router:[
-        {
-          path: '/index',
-          name: 'home',
-          component: r => require.ensure([], () => r(require('@/components/web/view/mode_preview')), 'list'),
-          meta: { title: '首页' , keepAlive:true},
-        },
+        // {
+        //   path: '/index',
+        //   name: 'home',
+        //   component: r => require.ensure([], () => r(require('@/components/web/view/mode_preview')), 'list'),
+        //   meta: { title: '首页' , keepAlive:true},
+        // },
     ],
 }
