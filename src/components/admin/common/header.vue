bed39f1d062ae5914a1007f9f9d88a4385240620
904678748@qq.com <904678748@qq.com>
2022-09-21 13:46:12 +0800

    ....

--- src/components/admin/common/header.vue
+++ src/components/admin/common/header.vue
@@ -103,12 +103,20 @@ export default {
       width: 100%;
       display: flex;
       align-items: center;
+      img{
+        max-height: 35px;
+        max-width: 185px;
+      }
     }
   }
   .logo-collapse{
     width: 80px;
     a{
       margin-left: 4px;
+      img{
+        max-height: 35px;
+        max-width: 185px;
+      }
     }
   }
   .m-cut{
