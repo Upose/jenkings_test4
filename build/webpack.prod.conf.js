153388518df6b6c550459247f13570c9052dce48
904678748@qq.com <904678748@qq.com>
2023-01-03 10:23:23 +0800

    css分离

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -23,11 +23,10 @@ const webpackConfig = merge(baseWebpackConfig, {
     })
   },
   devtool: config.build.productionSourceMap ? config.build.devtool : false,
+  
   output: {
     path: config.build.assetsRoot,
     publicPath: './',
-    // filename: utils.assetsPath('js/[name].js'),
-    // chunkFilename: utils.assetsPath('js/[id].js')
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
@@ -48,11 +47,6 @@ const webpackConfig = merge(baseWebpackConfig, {
     // extract css into its own file
     new ExtractTextPlugin({
       filename: utils.assetsPath('css/[name].[chunkhash].css'),
-      // filename: utils.assetsPath('css/[name].[contenthash].css'),
-      // Setting the following option to `false` will not extract CSS from codesplit chunks.
-      // Their CSS will instead be inserted dynamically with style-loader when the codesplit chunk has been loaded by webpack.
-      // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`, 
-      // increasing file size: https://github.com/vuejs-templates/webpack/issues/1110
       allChunks: true,
     }),
     // Compress extracted CSS. We are using this plugin so that possible
--- src/views/admin/preview/preview.vue
+++ src/views/admin/preview/preview.vue
@@ -67,57 +67,6 @@ export default {
 </script>
 
 <style lang="less">
-/*****这块区域主要针对背景图/视频/banner图滚动展示等 */
-.dlib3-temp-imgvideo {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  z-index: 1;
-  .banner-warp-bg,
-  .img-warp-bg,
-  .video-warp-bg {
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 40px;
-    color: #fff;
-  }
-  .img-warp-bg {
-    z-index: 2;
-  }
-  .video-warp-bg {
-    z-index: 3;
-    video {
-      width: 100%;
-      height: 100%;
-      background: #000;
-    }
-  }
-  .banner-warp-bg {
-    z-index: 4;
-    // background: rgba(255, 255, 255, 0.5);
-    #dlib3_bg_banner_component {
-      width: 100%;
-      height: 100%;
-      position: relative;
-      .dlib3-banner-index {
-        position: absolute;
-        z-index: 0;
-        width: 100%;
-        height: 100%;
-        overflow: hidden;
-        img {
-          width: 100%;
-          height: 100%;
-          object-fit: cover;
-          background: #fff;
-        }
-      }
-    }
-  }
-}
+@import "./css/publicGlobal.less";
 </style>
 
