f5015baa9a2eb2cdd4ccfa06d1ebdeec52016b53
904678748@qq.com <904678748@qq.com>
2023-01-16 10:30:14 +0800

    删除多余文件

--- config/cdlg.env.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-// 重庆大学环境
-module.exports = {
-  NODE_ENV: '"cdlg"',
-  VUE_APP_FILE_URL: 'http://192.168.250.81:80/cdn/'
-  // VUE_APP_BASE_API:'"http://192.168.21.46:8000/"',//api域名
-  // VUE_APP_IMG_URL:'"http://192.168.21.46:6900/"',//文件站点域名
-}
