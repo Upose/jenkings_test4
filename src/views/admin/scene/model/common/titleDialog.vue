ca0215bdfcadb2e8ea404374ce66fd55a7b7aecd
904678748@qq.com <904678748@qq.com>
2023-02-09 15:48:04 +0800

    ...

--- src/views/admin/scene/model/common/titleDialog.vue
+++ src/views/admin/scene/model/common/titleDialog.vue
@@ -14,16 +14,16 @@
           <el-form-item label="主标题" prop="name" class="title-row">
             <el-input v-model="postForm.mtitle" placeholder="请输入主标题" maxlength="10" minlength="0" show-word-limit></el-input>
             <el-color-picker v-model="postForm.mcolor" size="small"></el-color-picker>
-            <el-select class="f-size-input" size="small" v-model="postForm.mfsize" placeholder="14px">
+            <!-- <el-select class="f-size-input" size="small" v-model="postForm.mfsize" placeholder="14px">
               <el-option v-for="item in fonts" :key="item" :label="item" :value="item"></el-option>
-            </el-select>
+            </el-select> -->
           </el-form-item>
           <el-form-item label="副标题" prop="name" class="title-row">
             <el-input v-model="postForm.stitle" placeholder="请输入副标题" maxlength="10" minlength="0" show-word-limit></el-input>
             <el-color-picker v-model="postForm.scolor" size="small"></el-color-picker>
-            <el-select class="f-size-input" size="small" v-model="postForm.sfsize" placeholder="12px">
+            <!-- <el-select class="f-size-input" size="small" v-model="postForm.sfsize" placeholder="12px">
               <el-option v-for="item in fonts" :key="item" :label="item" :value="item"></el-option>
-            </el-select>
+            </el-select> -->
           </el-form-item>
           <el-form-item class="m-center">
             <el-button icon="iconfont el-icon-vip-baocun1" size="medium" type="primary" @click="submitForm()">保存</el-button>
@@ -34,18 +34,19 @@
   </div>
 </template>
 <style lang="less" scoped>
-.f-size-input{
-  position: absolute;
-  right:40px;
-  /deep/.el-input--small .el-input__inner{
-    padding-right: 0;
-    padding-left:2px;
-    width: 35px;
-  }
-  /deep/.el-input__suffix{
-    display: none;
-  }
-}
+/***字体设置 */
+// .f-size-input{
+//   position: absolute;
+//   right:40px;
+//   /deep/.el-input--small .el-input__inner{
+//     padding-right: 0;
+//     padding-left:2px;
+//     width: 35px;
+//   }
+//   /deep/.el-input__suffix{
+//     display: none;
+//   }
+// }
 </style>
 
 <script>
@@ -68,7 +69,7 @@ export default {
     return {
       fileUrl: window.localStorage.getItem('fileUrl'),
       dialogBulk:true,
-      fonts: ['12px','14px','16px','18px','20px','22px','24px','26px','28px','30px','36px','40px','50px','60px'],
+      // fonts: ['12px','14px','16px','18px','20px','22px','24px','26px','28px','30px','36px','40px','50px','60px'],
       postForm: {
         fontsize:'14px',
         bgimg: '',
@@ -105,8 +106,12 @@ export default {
 .title-row{
   position: relative;
   /deep/.el-input__suffix{
-    right:80px;
+    right:45px;
   }
+  // 设置标题组件字体大小
+  // /deep/.el-input__suffix{
+  //   right:80px;
+  // }
   .el-color-picker{
     position: absolute;
     right: 4px;
