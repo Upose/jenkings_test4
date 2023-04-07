787b279bd5ef470b5f2ae1c0de6e70598c4927b3
904678748@qq.com <904678748@qq.com>
2023-04-07 13:48:43 +0800

    .

--- src/views/admin/preview/preview.vue
+++ src/views/admin/preview/preview.vue
@@ -1,11 +1,3 @@
-<!--
- * @Author: 904678748@qq.com 904678748@qq.com
- * @Date: 2023-03-31 17:53:00
- * @LastEditors: 904678748@qq.com 904678748@qq.com
- * @LastEditTime: 2023-04-07 13:48:22
- * @FilePath: \home_sys\src\views\admin\preview\preview.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <!--预览页面选择模板-->
 <template>
   <div :style="{'background':bgColor,'min-width':minWidth+'px','height':min_height}">
