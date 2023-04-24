ce01a79be6d9d87abd81baa40cc403170f3b869f
904678748@qq.com <904678748@qq.com>
2023-04-24 17:10:40 +0800

    新版本

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: 904678748@qq.com 904678748@qq.com
+ * @Date: 2023-04-13 15:28:23
+ * @LastEditors: 904678748@qq.com 904678748@qq.com
+ * @LastEditTime: 2023-04-24 17:10:18
+ * @FilePath: \home_sys\src\router\index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import Vue from 'vue'
 import Router from 'vue-router'
 import webRouter from './web_router';
@@ -5,8 +13,8 @@ import adminRouter from './admin_router';
 Vue.use(Router)
 const dlibAppRouterPrefix = window.dlibAppRouterPrefix;
 const ruterBase = dlibAppRouterPrefix
-  ? dlibAppRouterPrefix + "/scenemanagenew/"
-  : "/scenemanagenew/";
+  ? dlibAppRouterPrefix + "/scenemanage/"
+  : "/scenemanage/";
 export default new Router({
   mode: 'history',
   base: process.env.NODE_ENV == "production" ? ruterBase : "",
